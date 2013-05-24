f2199827937326f9703592dde267ec4d884b6c30
David Goldschmidt <dmgoldschmidt@gmail.com>
2013-05-24 13:24:07 -0700

    New version of Array.h is running.  Uses compile-time allocation via template parameter N.  Future: specialization for N = 0: use run-time allocation.

--- include/Array.h
+++ include/Array.h
@@ -2,109 +2,130 @@
 #define ARRAY_H
 #include <iostream>
 #include <cassert>
-#define DEBUG
+#include "util.h"
+//#define DEBUG
 
 #ifdef DEBUG
-#include <string>
-using namespace std;
+#define DBG(x) std::cout << x;
+	 //#include <string>
+	 //using namespace std;
+#else
+#define DBG(x)
 #endif
 
-#define REFCOUNT (((int*)buf)[0])
-#define N (((int*)buf)[1])
-#define DATA ((ITEM*)(buf+Hlen*sizeof(int)/sizeof(ITEM) + 1)) // Note: is this right?
-template<typename ITEM, int Hlen = 2> // Hlen is the no. of integer header items
+
+
+template <typename ITEM, size_t N>
+class Array;
+
+template <typename ITEM, size_t N>
+class ArrayBlock{ // linked list of data blocks and control info
+  friend class Array<ITEM,N>;
+  ITEM data[N];
+  //  uint start;
+  ArrayBlock* next;
+  uint refcount;
+  ITEM fill;
+  bool filling;
+
+  ArrayBlock(void) : next(nullptr), refcount(0), filling(false) {}
+  ArrayBlock(const ITEM& f) : fill(f), next(nullptr), refcount(0), filling(true) {
+    for(uint i = 0;i < N;i++)data[i] = fill;
+  }
+  ArrayBlock(const ArrayBlock&); // no copy
+  ArrayBlock& operator=(const ArrayBlock&); //no assignment
+
+  ~ArrayBlock(void){
+    if(next != nullptr)delete next;
+  }
+
+  uint len(void){
+    return N + (next == nullptr? 0: next->len());
+  }
+
+  ITEM& operator[](uint i){
+    DBG(format("ArrayBlock %x: operator [%d]\n",this,i))
+    if(i >= N){ // on to next block
+      DBG(format("index %d exceeds block length\n",i))
+      fflush(stdout);
+      if(next == nullptr){
+	next = filling? new ArrayBlock(fill): new ArrayBlock();
+	DBG(format("new ArrayBlock at %x\n",next))
+	fflush(stdout);
+      }
+      return next->operator[](i-N);
+      
+    }
+    return data[i];
+  }
+};
+
+template<typename ITEM, size_t N> 
 class Array {
   //  friend ostream& operator <<(ostream& os, const Array& A);
 protected:
-  ITEM* buf;
+  ArrayBlock<ITEM,N>* first;
 public:
-  Array(void) : buf(NULL){}
-  Array(int n) : buf(NULL){reset(n);}
-  Array(int n, ITEM fill) : buf(NULL){reset(n,fill);}
-  Array(int n, const ITEM* p): buf(NULL){
-    reset(n);
-    for(int i = 0;i < n;i++) DATA[i] = p[i];
+  Array(void) : first(new ArrayBlock<ITEM,N>()){
+    DBG(format("Array: first block at %x\n",first))
   }
-  void reset(int n, ITEM fill){
-    reset(n);
-    for(int i = 0;i < n;i++){
-      cout << format("copying %d bytes of fill to %x\n",sizeof(fill), &DATA[i]);
-      DATA[i] = fill;
-    }
+
+  Array(const ITEM& fill) : first(new ArrayBlock<ITEM,N>(fill)){
+    DBG(format("Array: first block at %x, filled with ",first) << str(fill))
   }
-  void reset(int n){
-#ifdef ARRAY_BOUNDS
-    if(n < 0) throw "Array: negative length\n";
-#endif
-    if(buf != NULL){
-      if(n == N) return; //NOP
-      if(--REFCOUNT <= 0)delete[] buf;
-    }
-    int nitems = n + Hlen*sizeof(int)/sizeof(ITEM) + 1;
-    buf = new ITEM[nitems];
-    cout << format("allocated Array of %d items of length %d at %x\n",nitems,sizeof(ITEM),buf); 
-    REFCOUNT = 1;
-    N = n;
-  }
-  Array(const Array& A): buf(A.buf) {if(buf != NULL)REFCOUNT++;} // shallow copy
+
+
+  Array(const Array& A): first(A.first) {first->refcount++;} // shallow copy
   ~Array(void){
-    cout << format("Array destructor: buf =  %x, refcount = %d\n",buf,REFCOUNT); 
-    try{
-      if(buf != NULL && --REFCOUNT <= 0)delete[] buf;
-    }
-    catch(const exception& e) {
-      //      cout << "got exception: "<<e<<endl;
-      cout << format("error at buf = %x, refcount = %d\n",buf,REFCOUNT);
-    }
+    //    DBG("Array destructor: first =  %x, refcount = %d\n",first,first->refcount) 
+    if( first->refcount-- <= 1)delete first;
   }
-  Array& operator=(const Array& A){ // shallow copy
-    if(buf != NULL && --REFCOUNT <= 0)delete[] buf;
-    buf = A.buf;
-    if(buf != NULL) REFCOUNT++;
+  Array& operator=(const Array& A){ // shallow copy -- switch pointer to new first block
+    if(first->refcount-- <= 1)delete first;
+    first = A.first;
+    first-> refcount++;
     return *this;
   }
   Array copy(void) const{ // deep copy returning object
-    if(buf == NULL){
-      Array A;
-      return A;
-    }
-    Array A(N);
-    for(int i = 0;i < N;i++) A[i] = DATA[i];
+    Array<ITEM,N> A;
+    int n = len(); // only call recursive len once
+    for(int i = 0;i < n;i++) A[i] = this->operator[](i);
     return A;
   }
-  ITEM& operator[](int i){
-#ifdef ARRAY_BOUNDS
-    if(buf == NULL) throw "Array: can't access uninitialized Array\n";
-    if(i < 0 || i >= N) throw "Array: index out of bounds\n";
-#endif
-    return DATA[i];
-  }
+  int len(void) const {return first->len();}
+  ITEM& operator[](int i)const {return first->operator[](i);}
   bool operator==(Array& A) const{
-    if(N != ((int*)(A.buf))[1])return false;
-    for(int i = 0;i < N;i++) if(DATA[i] != A[i])return false;
+    int n = len();
+    if(n != A.len())return false;
+    for(int i = 0;i < n;i++) if(this->operator[](i) != A[i])return false;
     return true;
   }
   bool operator!=(Array& A) const{return !operator==(A);}
-  int len(void) const {
-    if(buf == NULL) return 0;
-    return N;
-  }
-
-#ifdef DEBUG
-  void dump(string s = ""){
-    cout << s<<": refcount: "<<REFCOUNT<<" N: "<<N<<" DATA: ";
-    for(int i = 0;i < N;i++)cout << DATA[i]<<" ";
-    cout << endl;
-  }
-#endif
-
 };
-template<typename ITEM>
-std::ostream& operator <<(std::ostream& os, Array<ITEM>& A){
-  for(int i = 0;i < A.len();i++)os << A[i]<<" ";
+
+template<typename ITEM, size_t N>
+std::ostream& operator <<(std::ostream& os, Array<ITEM,N>& A){
+  int n = A.len();
+  for(int i = 0;i < n;i++)os << A[i]<<" ";
   return os;
 } 
 
-
 #endif
+/*#define REFCOUNT (((int*)buf)[0])
+#define N (((int*)buf)[1])
+#define DATA ((ITEM*)(buf+Hlen*sizeof(int)/sizeof(ITEM) + 1)) // Note: is this right?
+*/
   
+  // void reset(const ITEM* p){first->reset(p);}
+  // void reset(const ITEM& fill){first->reset(fill);}
+  // void reset(void){first->reset();}
+  //   if(first != nullptr){
+  //     if(n == N) return; //NOP
+  //     if(--REFCOUNT <= 0)delete[] buf;
+  //   }
+  //   int nitems = n + Hlen*sizeof(int)/sizeof(ITEM) + 1;
+  //   buf = new ITEM[nitems];
+  //   DBG << format("allocated Array of %d items of length %d at %x\n",nitems,sizeof(ITEM),buf); 
+  //   REFCOUNT = 1;
+  //   N = n;
+  // }
