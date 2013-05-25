f640da3a85cc4f5b216afac9d08dbce934965326
David Goldschmidt <dmgoldschmidt@gmail.com>
2013-05-25 08:38:44 -0700

    Removed compile-time allocation because the run-time option is just as efficient.  Also, the UI is now basically unchanged, except that fill is provided by a const ITEM*.  This is a bit clumsy, but simplifies the code.

--- include/Array.h
+++ include/Array.h
@@ -15,85 +15,136 @@
 
 
 
-template <typename ITEM, size_t N>
+template <typename ITEM>
 class Array;
 
-template <typename ITEM, size_t N>
-class ArrayBlock{ // linked list of data blocks and control info
-  friend class Array<ITEM,N>;
-  ITEM data[N];
-  //  uint start;
+// template <typename ITEM, size_t N>
+// class ArrayBlock{ // linked list of data blocks and control info
+//   friend class Array<ITEM,N>;
+//   ITEM data[N];
+//   //  uint start;
+//   ArrayBlock* next;
+//   uint refcount;
+//   ITEM fill;
+//   bool filling;
+
+//   ArrayBlock(void) : next(nullptr), refcount(0), filling(false) {}
+//   ArrayBlock(const ITEM& f) : fill(f), next(nullptr), refcount(0), filling(true) {
+//     for(uint i = 0;i < N;i++)data[i] = fill;
+//   }
+//   ArrayBlock(const ArrayBlock&); // no copy
+//   ArrayBlock& operator=(const ArrayBlock&); //no assignment
+
+//   ~ArrayBlock(void){
+//     if(next != nullptr)delete next;
+//   }
+
+//   uint len(void){
+//     return N + (next == nullptr? 0: next->len());
+//   }
+
+//   ITEM& operator[](uint i){
+//     DBG(format("ArrayBlock %x: operator [%d]\n",this,i))
+//     if(i >= N){ // on to next block
+//       DBG(format("index %d exceeds block length\n",i))
+//       fflush(stdout);
+//       if(next == nullptr){
+// 	next = filling? new ArrayBlock(fill): new ArrayBlock();
+// 	DBG(format("new ArrayBlock at %x\n",next))
+// 	fflush(stdout);
+//       }
+//       return next->operator[](i-N);
+      
+//     }
+//     return data[i];
+//   }
+// };
+
+template <typename ITEM>
+class ArrayBlock{ 
+  friend class Array<ITEM>; // private helper class for Array
+  size_t n;
+  ITEM *data;
   ArrayBlock* next;
   uint refcount;
-  ITEM fill;
-  bool filling;
+  const ITEM* fill;
 
-  ArrayBlock(void) : next(nullptr), refcount(0), filling(false) {}
-  ArrayBlock(const ITEM& f) : fill(f), next(nullptr), refcount(0), filling(true) {
-    for(uint i = 0;i < N;i++)data[i] = fill;
+  ArrayBlock(size_t nn, const ITEM* f) : n(nn), next(nullptr), refcount(0), fill(f) {
+    data = new ITEM[n];
+    if(fill != nullptr)
+      for(uint i = 0;i < n;i++)data[i] = *fill;
   }
   ArrayBlock(const ArrayBlock&); // no copy
   ArrayBlock& operator=(const ArrayBlock&); //no assignment
 
   ~ArrayBlock(void){
+    delete[] data;
     if(next != nullptr)delete next;
   }
 
   uint len(void){
-    return N + (next == nullptr? 0: next->len());
+    return n + (next == nullptr? 0: next->len());
   }
 
   ITEM& operator[](uint i){
     DBG(format("ArrayBlock %x: operator [%d]\n",this,i))
-    if(i >= N){ // on to next block
+    if(i >= n){ // on to the next block
       DBG(format("index %d exceeds block length\n",i))
       fflush(stdout);
       if(next == nullptr){
-	next = filling? new ArrayBlock(fill): new ArrayBlock();
+	next = new ArrayBlock(n,fill);
 	DBG(format("new ArrayBlock at %x\n",next))
-	fflush(stdout);
       }
-      return next->operator[](i-N);
+      return next->operator[](i-n);
       
     }
     return data[i];
   }
 };
 
-template<typename ITEM, size_t N> 
+template<typename ITEM> 
 class Array {
   //  friend ostream& operator <<(ostream& os, const Array& A);
 protected:
-  ArrayBlock<ITEM,N>* first;
+  ArrayBlock<ITEM>* first; // first (and possibly only) block of data and control info
 public:
-  Array(void) : first(new ArrayBlock<ITEM,N>()){
-    DBG(format("Array: first block at %x\n",first))
+  Array(void) : first(nullptr){}
+  Array(size_t n, const ITEM* fill = nullptr) : first(new ArrayBlock<ITEM>(n,fill)){
+    DBG(format("Array: first block at %x",first))
   }
-
-  Array(const ITEM& fill) : first(new ArrayBlock<ITEM,N>(fill)){
-    DBG(format("Array: first block at %x, filled with ",first) << str(fill))
-  }
-
-
-  Array(const Array& A): first(A.first) {first->refcount++;} // shallow copy
+  Array(const Array& A): first(A.first) {if(first != nullptr) first->refcount++;} // shallow copy
   ~Array(void){
     //    DBG("Array destructor: first =  %x, refcount = %d\n",first,first->refcount) 
-    if( first->refcount-- <= 1)delete first;
+    if( first != nullptr && first->refcount-- <= 1)delete first;
   }
+  void reset(size_t n, const ITEM* fill = nullptr){
+    if(first != nullptr && first->refcount-- <= 1)delete first;
+    first = new ArrayBlock<ITEM>(n,fill);
+  }
+
   Array& operator=(const Array& A){ // shallow copy -- switch pointer to new first block
-    if(first->refcount-- <= 1)delete first;
+    if(first != nullptr && first->refcount-- <= 1)delete first;
     first = A.first;
     first-> refcount++;
     return *this;
   }
-  Array copy(void) const{ // deep copy returning object
-    Array<ITEM,N> A;
-    int n = len(); // only call recursive len once
-    for(int i = 0;i < n;i++) A[i] = this->operator[](i);
+  Array copy(void) const{ // deep copy returning object 
+    Array<ITEM> A;
+    if(first != nullptr){
+      A.reset(first->n,first->fill);
+      int n = len(); // only call recursive len once
+      for(int i = 0;i < n;i++) A[i] = this->operator[](i);
+    }
     return A;
   }
-  int len(void) const {return first->len();}
-  ITEM& operator[](int i)const {return first->operator[](i);}
+  int len(void) const {
+    if(first == nullptr) return 0;
+    return first->len();
+  }
+  ITEM& operator[](int i)const {
+    if(first == nullptr)throw "Array bounds error\n";
+    return first->operator[](i);
+  }
   bool operator==(Array& A) const{
     int n = len();
     if(n != A.len())return false;
@@ -103,8 +154,8 @@ public:
   bool operator!=(Array& A) const{return !operator==(A);}
 };
 
-template<typename ITEM, size_t N>
-std::ostream& operator <<(std::ostream& os, Array<ITEM,N>& A){
+template<typename ITEM>
+std::ostream& operator <<(std::ostream& os, Array<ITEM>& A){
   int n = A.len();
   for(int i = 0;i < n;i++)os << A[i]<<" ";
   return os;
